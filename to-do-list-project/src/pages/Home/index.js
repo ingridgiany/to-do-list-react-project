784ef77bbe2e32d8bda9e421960ecef356c324a6
Ingrid Giany <ingridgianys@gmail.com>
2023-10-12 01:06:46 +0200

    Criando página home

--- to-do-list-project/src/pages/Home/index.js
+++ to-do-list-project/src/pages/Home/index.js
@@ -1,9 +1,48 @@
+import { useState } from "react"
+import './home.css'
+
+import { Link } from "react-router-dom"
+
+export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLogin(e){
+    e.preventDefault()
+
+    if(email !== '' && password !== ''){
+      alert('TEST')
+    }else {
+      alert('Fill in all fields')
+    }
 
 
-export default function Home(){
-    return(
-      <div>
-        <h1>Página Inicial</h1>
-      </div>
-    )
   }
+
+  return (
+    <div className="home-container">
+      <h1>To-do list</h1>
+      <span>Manage your schedule easily...</span>
+
+      <form className="form" onSubmit={handleLogin}>
+        <input type="text"
+          placeholder="Type your email"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value) }} />
+
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => { setPassword(e.target.value) }} />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <Link className="button-link" to='/register'>
+        Don't have an account? Register
+      </Link>
+    </div>
+  )
+}
