2071caf8152ef6c00f25909f62c2377c3943b843
Ingrid Giany <ingridgianys@gmail.com>
2023-10-12 03:11:55 +0200

    Criando página de admin

--- to-do-list-project/src/pages/admin/index.js
+++ to-do-list-project/src/pages/admin/index.js
@@ -1,9 +1,47 @@
 import './admin.css'
+import { useState } from 'react'
 
-export default function Admin(){
-    return(
-        <div>
-            <h1>Admin</h1>
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
+
+export default function Admin() {
+    const [taksInput, setTaksInput] = useState('')
+
+    function handleTask(e){
+        e.preventDefault()
+
+        alert('CLICK')
+    }
+
+    async function handleLogout(){
+        await signOut(auth)
+    }
+
+    return (
+        <div className='admin-container'>
+            <h1>My tasks</h1>
+
+            <form className='form' onSubmit={handleTask}>
+                <textarea
+                    placeholder='Enter your tasks for today...' 
+                    value={taksInput}
+                    onChange={(e)=> setTaksInput(e.target.value)}
+                    />
+
+                <button className='btn-add' type='submit'>Add task</button>
+            </form>
+
+            <article className='list'>
+                <p>Terminar de assistir 6 vídeos do curso hoje.</p>
+
+                <div>
+                    <button>Edit</button>
+                    <button className='btn-done'>Done</button>
+                </div>
+
+            </article>
+
+            <button onClick={handleLogout} className='btn-logout'>Logout</button>
         </div>
     )
 }
