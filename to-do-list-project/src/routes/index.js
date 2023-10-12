78de5cdcba187967a0d58eccf959fb28c4a1ad9e
Ingrid Giany <ingridgianys@gmail.com>
2023-10-12 02:26:49 +0200

    Controle de login do usuário

--- to-do-list-project/src/routes/index.js
+++ to-do-list-project/src/routes/index.js
@@ -4,13 +4,14 @@ import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Admin from "../pages/admin";
 
+import Private from "./Private";
 
 function RoutesApp(){
     return(
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/register' element={<Register />}/>
-            <Route path='/admin' element={<Admin />}/>
+            <Route path='/admin' element={<Private><Admin /></Private>}/>
         </Routes>
     )
 }
